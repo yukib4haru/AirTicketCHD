0c4ae7b60320ebfb4f05bf4528517e050e7a1a8d
HYSSNH <130443526+HYSSNH@users.noreply.github.com>
2023-12-06 21:37:16 +0800

    graph

--- yonghu.cpp
+++ yonghu.cpp
@@ -9,25 +9,42 @@ yonghu::yonghu(QWidget *parent) :
 {
     ui->setupUi(this);
     this->setWindowTitle("Login");
+
+    this->setAutoFillBackground(true);
+    //获得widget的palette
+    QPalette palette = this->palette();
+    palette.setBrush(QPalette::Window,
+            QBrush(QPixmap(":/background.png").scaled(  // 缩放背景图.
+                this->size(),Qt::IgnoreAspectRatio,        //不限制原图片的长宽比
+                Qt::SmoothTransformation)));               // 使用平滑的缩放方式
+    this->setPalette(palette);
+
+
     this->setGeometry(750,300,400,300);
     this->setFixedSize(400,300);
 
     btnA = new QPushButton(this);
     btnA->setText("登录");// 设置按钮的文本
     btnA->setGeometry(65, 200, 100, 50);   // 设置按钮的位置和大小
+    btnA->setFlat(true);
+    btnA->setStyleSheet("background-color: rgb(255,250,250);color: rgb(0,255,0);");
     btnA->show();
 
     btnB = new QPushButton(this);
     btnB->setText("注册");// 设置按钮的文本
     btnB->setGeometry(225, 200, 100, 50);   // 设置按钮的位置和大小
+    btnB->setFlat(true);
+    btnB->setStyleSheet("background-color: rgb(255,250,250);color: rgb(0,0,0);");
     btnB->show();
 
     tel_num = new QLabel(this);
     tel_num->setText("Tel_Num:");
     tel_num->setGeometry(50, 45, 68, 25);
+    tel_num->setStyleSheet("QLabel{color: rgb(255,0,0);}");
     password = new QLabel(this);
     password->setText("PassWord:");
     password->setGeometry(50, 105, 68, 25);
+    password->setStyleSheet("QLabel{color: rgb(255,0,0);}");
 
     tel_num_lineEdit = new QLineEdit(this);
     tel_num_lineEdit->setGeometry(160, 45, 170, 30);
@@ -76,7 +93,6 @@ yonghu::yonghu(QWidget *parent) :
             case 0:
             { QMessageBox::information(this,"信息","登录成功！");
                 this->close();
-
 
                 MainWindow *w = new MainWindow;
                 w->show();
